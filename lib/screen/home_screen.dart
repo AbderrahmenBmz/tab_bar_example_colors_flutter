5062cca9db93a130be194ddabca9eee25cfec39b
AbderrahmenBmz <abdogray@gmail.com>
2020-05-02 02:33:51 +0100

    add controller

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -5,14 +5,62 @@ class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _HomeScreenState extends State<HomeScreen>
+    with SingleTickerProviderStateMixin {
+  TabController _tabController;
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(initialIndex: 0, length: 3, vsync: this);
+  }
+
+  Widget _slidColor(Color color, String name) {
+    return Container(
+      color: color,
+      child: Center(
+        child: Text(
+          name,
+          style: TextStyle(fontSize: 80),
+        ),
+      ),
+    );
+  }
+
+  Widget _tab(String name) {
+    return Tab(
+      text: name,
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _tabController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Colors')),
-      );
-      
-   
+          title: Text('Colors'),
+          centerTitle: true,
+          bottom: TabBar(
+            indicatorColor: Colors.white,
+            tabs: [
+              _tab('Teal'),
+              _tab('Blue'),
+              _tab('White'),
+            ],
+            controller: _tabController,
+          )),
+      body: TabBarView(
+        children: [
+          _slidColor(Colors.teal, 'Teal'),
+          _slidColor(Colors.blue, 'Blue'),
+          _slidColor(Colors.white, 'White'),
+        ],
+        controller: _tabController,
+      ),
+    );
   }
 }
